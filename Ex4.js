c826214bc3fd3e2048007330e8970fe5b39fd8e9
SamsonNicoleta <nicoletasamson194@gmail.com>
2023-03-02 14:49:57 +0200

    Ex.4

---
+++ Ex4.js
@@ -0,0 +1,26 @@
+const produs = [
+    {
+        "denumire": "lapte",
+        "pret": 15
+        "cantitate": 2
+    },
+    {
+        "denumire": "paine",
+        "pret": 10
+        "cantitate": 3
+    },
+    {
+        "denumire": "cereale",
+        "pret": 30
+        "cantitate": 4
+    }
+];
+
+console.log('All produs');
+console.log(students);
+
+let sum = 0;
+for(let key in pret){
+	sum += pret[key];
+}
+console.log(sum);
