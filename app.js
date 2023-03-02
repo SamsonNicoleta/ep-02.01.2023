ddc5adf6f3eeb4fdf472fd074c9a08e3e3b16254
SamsonNicoleta <nicoletasamson194@gmail.com>
2023-03-02 14:37:00 +0200

    Ex3

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function uppercaseFirstLetter(sentence) {
+/*function uppercaseFirstLetter(sentence) {
     let words = sentence.split(' ');
     for(let i = 0; i < words.length; i++) {
       words[i] = words[i].at(0).toUpperCase() + words[i].slice(1);
@@ -7,4 +7,14 @@ function uppercaseFirstLetter(sentence) {
 }
 
 let r = uppercaseFirstLetter("astazi este joi");
-console.log(r);
+console.log(r);*/
+
+function squareNegativeNumbers(array) {
+    let negativeNumbers = array.filter(element => element < 0);
+    let squaredNegativNumbers = negativNumbers.map(elem => element * element);
+  
+    return squaredNegativNumbers;
+}
+
+let n = [3, -2, 4, -4, 4, 16, -3, 10, 9, 15, -1, 6];
+console.log(squareNegativNumbers(n));
